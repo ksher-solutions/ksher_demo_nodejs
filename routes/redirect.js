961da2ab3d386cdbc93b013e1b6cf76e0f493779
nutchac <nutchac1@hotmail.com>
2023-09-25 15:37:44 +0700

    add refund website

--- routes/redirect.js
+++ routes/redirect.js
@@ -1,30 +1,28 @@
-var express = require('express');
-const KsherPay = require('@kshersolution/ksher');
-var bodyParser = require('body-parser')
-const config = require('./config')
+var express = require("express");
+const KsherPay = require("@kshersolution/ksher");
+var bodyParser = require("body-parser");
+const config = require("./config");
 var router = express.Router();
 
-const sdk = new KsherPay(config.appid, config.privatekey)
+const sdk = new KsherPay(config.appid, config.privatekey);
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('redirect', { title: 'Example use for Website' });
+router.get("/", function (req, res, next) {
+  res.render("redirect", { title: "Example use for Website" });
 });
 
-router.get('/success_page', function(req, res) {
-  
-  res.render('success_page', { title: 'Ksher' });
+router.get("/success_page", function (req, res) {
+  res.render("success_page", { title: "Ksher" });
 });
 
-router.get('/fail_page', function(req, res) {
-  
-  res.render('fail_page', { title: 'Ksher' });
+router.get("/fail_page", function (req, res) {
+  res.render("fail_page", { title: "Ksher" });
 });
 
-router.all('/create', async function(req, res,next) {
+router.all("/create", async function (req, res, next) {
   var channel_list = req.body.channel_list.join();
   const requestBody = {
-    total_fee : req.body.total_fee,
+    total_fee: req.body.total_fee,
     fee_type: req.body.fee_type,
     mch_order_no: req.body.mch_order_no,
     product_name: req.body.product_name,
@@ -36,38 +34,71 @@ router.all('/create', async function(req, res,next) {
     refer_url: req.body.refer_url,
     attach: req.body.attach,
   };
-  console.log("requestBody: ",requestBody);
+  console.log("requestBody: ", requestBody);
   try {
-    await sdk.gateway_pay(requestBody)
-    .then(response => {
-      console.log("--------------------",);
-      console.log("body: ",response);
-      var url=response.data.pay_content;
-      console.log("url: ",url);
-    res.redirect(url);
+    await sdk.gateway_pay(requestBody).then((response) => {
+      console.log("--------------------");
+      console.log("body: ", response);
+      var url = response.data.pay_content;
+      console.log("url: ", url);
+      res.redirect(url);
     });
   } catch (error) {
     return next(error);
   }
-  
 });
 
-router.all('/query', async function(req, res,next) {
+router.all("/query", async function (req, res, next) {
   const requestBody = {
-    mch_order_no: req.body.mch_order_no
+    mch_order_no: req.body.mch_order_no,
   };
-  console.log("requestBody: ",requestBody);
+  console.log("requestBody: ", requestBody);
   try {
-    await sdk.order_query(requestBody)
-    .then(response => {
-      console.log("--------------------",);
-      console.log("body: ",response);
-    res.send(response)
+    await sdk.gateway_order_query(requestBody).then((response) => {
+      console.log("--------------------");
+      console.log("body: ", response);
+      res.send(response);
     });
   } catch (error) {
     return next(error);
   }
-  
+});
+
+router.all("/refund", async function (req, res, next) {
+  const requestBody_query = {
+    mch_order_no: req.body.mch_order_no,
+  };
+  console.log("requestBody: ", requestBody_query);
+  try {
+    var response_query = await sdk.gateway_order_query(requestBody_query);
+    var ksher_order_refund = response_query.data.ksher_order_no;
+    if (response_query.data.result == "REFUND") {
+      // remove this if use partial refund
+      res.send("order have been refund!");
+    }
+    if (response_query.data.result != "SUCCESS") {
+      res.send("order don't paid, please make sure order have paid");
+    }
+    const requestBody_refund = {
+      ksher_order_no: ksher_order_refund,
+      mch_refund_no: req.body.mch_refund_no,
+      total_fee: req.body.total_fee,
+      fee_type: req.body.fee_type,
+      refund_fee: req.body.refund_fee,
+    };
+  } catch (error) {
+    return next(error);
+  }
+
+  try {
+    await sdk.order_refund(requestBody_refund).then((response_refund) => {
+      console.log("--------------------");
+      console.log("body: ", response_refund);
+      res.send(response_refund);
+    });
+  } catch (error) {
+    return next(error);
+  }
 });
 
 module.exports = router;
